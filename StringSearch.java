28d9734a6adf262d61a90f5aa2e171c26d951ae4
yeoheeim-ucsd <120440576+yeoheeim-ucsd@users.noreply.github.com>
2023-02-11 19:18:43 -0800

    Update StringSearch.java

--- StringSearch.java
+++ StringSearch.java
@@ -1,9 +1,6 @@
 import java.nio.file.*;
 import java.io.IOException;
 class FileHelper {
-    // Takes a path to a file as input
-    // Returns all of the lines in the file as an array of Strings
-    // Prints an error message if it failed
     static String[] getLines(String path) {
         try {
             return Files.readAllLines(Paths.get(path)).toArray(String[]::new);
